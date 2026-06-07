99d400ba8ee4557f88cfe03249d0b76accddd860
syu6noob <kusahebi@outlook.jp>
2026-06-07 22:30:38 +0900

    initial commit

---
+++ src/main/java/com/github/syu6noob/coordinates/Coordinates.java
@@ -0,0 +1,17 @@
+package com.github.syu6noob.coordinates;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Coordinates extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
